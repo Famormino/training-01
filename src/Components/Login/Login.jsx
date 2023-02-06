edc44b7861be617ca111a0b48899952688768341
Federico Amormino <federico.amormino@gmail.com>
2023-02-06 17:35:00 -0300

    adding disabled button

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./Login.module.css";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [emailIsValid, setEmailIsValid] = useState();
+    const [password, setPassword] = useState("");
+    const [passwordIsValid, setPasswordIsvalid] = useState();
+    const [formIsValid, setFormIsValid] = useState(false);
+
+    const onEmailChangeHandler = (event) => {
+        setEmail(event.target.value);
+
+        setFormIsValid(
+            event.target.value.includes("@") && password.trim().length > 6
+        );
+    };
+
+    const onPasswordChangeHandler = (event) => {
+        setPassword(event.target.value);
+
+        setFormIsValid(event.target.value.trim().length > 6 && email.includes("@"));
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(email, password, formIsValid);
+    };
+
     return (
         <Card className={classes.login}>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor="email">E-Mail</label>
-                    <input type="email" id="email" />
+                    <input
+                        type="email"
+                        id="email"
+                        onChange={onEmailChangeHandler}
+                    />
                 </div>
                 <div className={classes.control}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={onPasswordChangeHandler}
+                    />
                 </div>
                 <div className={classes.actions}>
-                    <Button type="submit">Login</Button>
+                    <Button type="submit" disabled={!formIsValid}>Login</Button>
                 </div>
             </form>
         </Card>
--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -7,6 +7,7 @@ const Button = (props) => {
             type={props.type || 'button'}
             className={`${classes.button} ${props.className}`}
             onClick={props.onClick}
+            disabled={props.disabled}
         >
             {props.children}
         </button>
