8d6942f915e6cec940b7947dfa80ca0388eddbc4
Federico Amormino <federico.amormino@gmail.com>
2023-02-06 11:23:51 -0300

    adding styles to login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
+import classes from "./Login.module.css";
 
 const Login = () => {
     return (
-        <Card>
-            <h1>Card</h1>
+        <Card className={classes.login}>
+            <form>
+                <div className={classes.control}>
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email" />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <div className={classes.actions}>
+                    <Button type="submit">Login</Button>
+                </div>
+            </form>
         </Card>
     );
 };
