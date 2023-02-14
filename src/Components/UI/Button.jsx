5aced4497cb1b0702054d50bcdb05b8244a679c2
Federico Amormino <federico.amormino@gmail.com>
2023-02-14 09:55:10 -0300

    slinteando

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -2,16 +2,16 @@ import React from "React";
 import classes from "./Button.module.css";
 
 const Button = (props) => {
-    return(
+    return (
         <button
-            type={props.type || 'button'}
+            type={props.type || "button"}
             className={`${classes.button} ${props.className}`}
             onClick={props.onClick}
             disabled={props.disabled}
         >
             {props.children}
         </button>
-    )
-}
+    );
+};
 
-export default Button
+export default Button;
